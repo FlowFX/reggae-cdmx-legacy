e70110fa6d8e99b3a5733910a133be6042532d33
Florian Posdziech <hallo@flowfx.de>
2016-04-10 13:11:08 -0500

    Add some Open Graph properties

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,7 +5,16 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+  <title><?php echo $site->title()->html() ?></title>
+  <meta name="description" content="<?php echo $site->text()->html() ?>">
+
+  <meta property="og:description" content="<?php echo $site->text() ?>">
+  <meta property="og:image" content="g">
+  <meta property="og:locale" content="es">
+  <meta property="og:site_name" content="<?php echo $site->title() ?>">
+  <meta property="og:title" content="<?php echo $site->title() ?>">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="<?php echo $site->url() ?>">
 
   <?php 
     setlocale(LC_ALL, 'es_MX.UTF-8');
