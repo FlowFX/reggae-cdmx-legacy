612a00d4316a32a223a3e356e94c422c6ec28d84
Florian Posdziech <hallo@flowfx.de>
2016-04-10 12:56:25 -0500

    Separate default and home template and controller

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,43 +1,10 @@
 <?php snippet('header') ?>
 
-
 <div class="column">
 
+<h1><?php echo $page->title()->html() ?></h1>
 
-  <h1><?php echo $site->title()->html() ?></h1>
-
-  <?php
-
-  foreach($calendar as $key => $year):
-
-    foreach($year as $key => $month):
-      echo "<h3>" . $key . "</h3>";
-
-      echo "<ul>";
-
-      foreach($month as $key => $event): ?>
-
-        <li class="event">
-
-          <span class="date">
-            <?php echo $event["date"] ?>
-          </span>
-
-          <a href="<?php echo $event["fbLink"] ?>">
-            <?php echo $event["title"] ?>
-          </a>
-
-        </li>
-
-      <?php endforeach ?>
-
-      </ul>
-
-    <?php endforeach ?>
-
-  <?php endforeach ?>
-
-
+<?php echo $page->text()->kirbytext() ?>
 
 </div> <!-- .column -->
 
