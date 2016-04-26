3e914be4584747c2e6b5350d4a78b02f7f225c66
Florian Posdziech <hallo@flowfx.de>
2016-04-26 16:09:34 -0500

    Show date only for first event of the day

--- site/templates/home.php
+++ site/templates/home.php
@@ -12,14 +12,29 @@
       foreach($month as $key => $week):
 
       echo "<ul>";
+      $i = 0;
 
-        foreach($week as $key => $event): ?>
+        foreach($week as $key => $event): 
+
+        //$i = array_search($key, array_keys($week));
+        $i++;
+          ?>
+
+          
+
 
           <li class="event">
 
+
+          
             <span class="date">
-              <?php echo $event["date"] ?>
+            <?php if($i == 1) {
+               echo $event["date"];
+            } else {
+              echo  str_repeat('&nbsp;', 10);
+            } ?>
             </span>
+          
 
           <a href="<?php echo $event["fbLink"] ?>">
           <?php
