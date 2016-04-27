99dd771bf5d92c3afd8cafa67aca13c914214908
Florian Posdziech <hallo@flowfx.de>
2016-04-27 15:31:53 -0500

    Enable memcached. But disable browser caching for css and js.

--- site/config/config.dev.reggae-cdmx.com.php
+++ site/config/config.dev.reggae-cdmx.com.php
@@ -1,3 +1,6 @@
 <?php
 
 c::set('cachebuster', true);
+
+c::set('cache', false);
+c::set('cache.driver', 'memcached');
--- site/config/config.reggae-cdmx.com.php
+++ site/config/config.reggae-cdmx.com.php
@@ -1,3 +1,6 @@
 <?php
 
 c::set('cachebuster', true);
+
+c::set('cache', true);
+c::set('cache.driver', 'memcached');
