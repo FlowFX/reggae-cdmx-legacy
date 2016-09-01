e0b97b5f1a6b99346fe6a0efc845ba98e3f819e8
Florian Posdziech <flowfx@lalibela.local>
2016-09-01 07:58:28 -0500

    Configure Cachebuster on new server

--- site/config/config.reggae-cdmx.com.php
+++ site/config/config.reggae-cdmx.com.php
@@ -7,5 +7,5 @@ c::set('thumbs.driver', 'im');
 c::set('cachebuster', true);
 
 c::set('cache', true);
-c::set('cache.driver', 'memcached');
+c::set('cache.driver', 'file');
 c::set('cache.ignore', array('sitemap'));
